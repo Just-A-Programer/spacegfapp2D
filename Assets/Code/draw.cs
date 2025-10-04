dbd0fc34e3f1df28fee36012f639b3582d4f26e1
Just-A-Programer <88325989+Just-A-Programer@users.noreply.github.com>
2025-10-04 14:59:51 +0300

    dqdwwq

---
+++ Assets/Code/draw.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class draw : MonoBehaviour
+{
+    public int drawID = -1;
+    public uint currFloor;
+    public GameObject[] SS_floor;
+    public GameObject[] SS_prefabs;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1)) { drawID = -1; }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && drawID != -1) { drawSS_mod(); }
+    }
+
+    void drawSS_mod()
+    {
+        Vector2 cursorclickPoss = Input.mousePosition;
+        Vector3 cursorPoss_temp = Camera.main.ScreenToWorldPoint(new Vector3(cursorclickPoss.x, cursorclickPoss.y, Camera.main.nearClipPlane));
+        Vector2 cursorPoss = new Vector2(cursorPoss_temp.x, cursorPoss_temp.y);
+
+        GameObject new_mod = GameObject.Instantiate(SS_prefabs[drawID]);
+        new_mod.transform.SetParent(SS_floor[currFloor].transform);
+        new_mod.transform.position = new Vector3(Mathf.Floor(cursorPoss.x), Mathf.Floor(cursorPoss.y), 0);
+
+    }
+
+    public void SetDrawID(int ID)
+    {
+        drawID = ID;
+    }
+}
