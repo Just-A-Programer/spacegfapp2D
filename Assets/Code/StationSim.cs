d91b8fc62aa36f970d70554d30b1164f92b34ac1
Just-A-Programer <88325989+Just-A-Programer@users.noreply.github.com>
2025-10-04 23:45:09 +0300

    SADWEWEFW

--- Assets/Code/StationSim.cs
+++ Assets/Code/StationSim.cs
@@ -35,17 +35,17 @@ public class StationSim : MonoBehaviour
         stats[1].color = new Color(Mathf.Lerp(0, 1, ((float)mass / (float)SSMS.TotalMaxMass)), 1 - Mathf.Lerp(0, 1, ((float)mass / (float)SSMS.TotalMaxMass)), 0); 
         if (mass >= SSMS.TotalMaxMass) { stats[1].color = new Color(0.5f, 0, 0); }
 
-        //Net Power Production
+        /*//Net Power Production
         stats[2].text = netpowprod + " W";
         if (netpowprod > 0) { stats[2].color = new Color(0, 1, 0); }
-        else { stats[2].color = new Color(1, 0, 0); }
+        else { stats[2].color = new Color(1, 0, 0); }*/
 
         //Morale
-        stats[3].text = morale + "%";
-        stats[3].color = new Color(1 - Mathf.Lerp(0, 1, ((float)morale / (float)100f)), Mathf.Lerp(0, 1, ((float)morale / (float)100f)), 0);
+        stats[2].text = morale + "%";
+        stats[2].color = new Color(1 - Mathf.Lerp(0, 1, ((float)morale / (float)100f)), Mathf.Lerp(0, 1, ((float)morale / (float)100f)), 0);
 
         //COS
-        stats[4].text = COS + "%";
-        stats[4].color = new Color(1 - Mathf.Lerp(0, 1, ((float)COS / (float)100f)), Mathf.Lerp(0, 1, ((float)COS / (float)100f)), 0);
+        stats[3].text = COS + "%";
+        stats[3].color = new Color(1 - Mathf.Lerp(0, 1, ((float)COS / (float)100f)), Mathf.Lerp(0, 1, ((float)COS / (float)100f)), 0);
     }
 }
--- Assets/Code/Timer.cs
+++ Assets/Code/Timer.cs
@@ -1,11 +1,15 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    public GameObject ENDSCREEN;
+    public Animator ENDSCREENANIM;
     public TextMeshProUGUI timertext;
     public SpaceStationModuleStats SSMS;
     float timer;
+    public float debug;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +22,8 @@ public class Timer : MonoBehaviour
         if (timer > 0) { timer -= Time.deltaTime; }
         if (timer < 0) { timer = 0; }
 
+        if (timer == 0) { WorldsEnd(); }
+
         if (Mathf.Floor(timer / 60) != 0)
             timertext.text = Mathf.Floor(timer / 60) + "m. " + Mathf.Ceil(timer % 60) + "s.";
         else
@@ -26,5 +32,26 @@ public class Timer : MonoBehaviour
         if (timer <= SSMS.TotalBuildTime && timer > 0.5 * SSMS.TotalBuildTime) { timertext.color = new Color(0, 1, 0); }
         else if (timer <= 0.5 * SSMS.TotalBuildTime && timer >= 0.1 * SSMS.TotalBuildTime) { timertext.color = new Color(1, 1, 0); }
         else { timertext.color = new Color(1, 0, 0); }
+    }
+
+    public void WorldsEnd()
+    {
+        //game stats
+        float budget    = SSMS.TotalCost;
+        float maxbudget = SSMS.TotalBudget;
+        float mass      = SSMS.TotalMass;
+        float maxmass   = SSMS.TotalMaxMass;
+        float morale    = SSMS.TotalMorale;
+        
+        float score = morale * (budget / maxbudget) * Mathf.Pow(mass / maxmass, -1);
+
+        score = debug;
+
+        ENDSCREEN.SetActive(true);
+
+        if (score >= 0.85) { ENDSCREENANIM.Play("win", 0); }
+        else if (score >= 0.50 && score < 0.85) { ENDSCREENANIM.Play("lose_long", 0); }
+        else if (score >= 0.25 && score < 0.50) { ENDSCREENANIM.Play("lose_medium", 0); }
+        else if (score < 0.25) { ENDSCREENANIM.Play("lose_short", 0); }
     }
 }
