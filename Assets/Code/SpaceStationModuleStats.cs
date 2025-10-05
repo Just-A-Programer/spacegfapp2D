7c5d92bc490d6bebba56ceb381d7e9c85dba7bc2
Just-A-Programer <88325989+Just-A-Programer@users.noreply.github.com>
2025-10-05 06:52:50 +0300

    GOOOOOOOOOOOOOOOOOOOOOOOODDD MORNING VIATNAM

--- Assets/Code/SpaceStationModuleStats.cs
+++ Assets/Code/SpaceStationModuleStats.cs
@@ -11,15 +11,12 @@ public class SpaceStationModuleStats : ScriptableObject
     public float TotalBuildTime;
     public float TotalMorale;
     public float TotalChanceOFSucsess;
-    public float TotalPowerProduction;
-    public float TotalPowerConsumption;
     public int TotalBudget;
     public int TotalCost;
     public string[] ModuleName;
     public int[] ModulePrice;
     public int[] ModuleMass;
     public float[] ModuleMorale;
-    public float[] ModulePowerConsumption;
     public Sprite[] ModuleSprite;
     public GameObject[] ModulePrefab;
 
@@ -30,7 +27,6 @@ public class SpaceStationModuleStats : ScriptableObject
         if (ModulePrice.Length != NumberOfModules)            { Array.Resize(ref ModulePrice, NumberOfModules); }
         if (ModuleMass.Length != NumberOfModules)             { Array.Resize(ref ModuleMass, NumberOfModules); }
         if (ModuleMorale.Length != NumberOfModules)           { Array.Resize(ref ModuleMorale, NumberOfModules); }
-        if (ModulePowerConsumption.Length != NumberOfModules) { Array.Resize(ref ModulePowerConsumption, NumberOfModules); }
         if (ModuleSprite.Length != NumberOfModules)           { Array.Resize(ref ModuleSprite, NumberOfModules); }
         if (ModulePrefab.Length != NumberOfModules)           { Array.Resize(ref ModulePrefab, NumberOfModules); }
     }
--- Assets/Code/StationSim.cs
+++ Assets/Code/StationSim.cs
@@ -10,7 +10,6 @@ public class StationSim : MonoBehaviour
     int budget;
     int cost;
     int mass;
-    float netpowprod;
     float morale;
     float COS;
 
@@ -20,9 +19,8 @@ public class StationSim : MonoBehaviour
         budget = SSMS.TotalBudget;
         cost = SSMS.TotalCost;
         mass = SSMS.TotalMass;
-        netpowprod = Mathf.Round(SSMS.TotalPowerProduction - SSMS.TotalPowerConsumption);
         morale = SSMS.TotalMorale;
-        COS = SSMS.TotalChanceOFSucsess;
+        //COS = SSMS.TotalChanceOFSucsess;
 
         //budget
         stats[0].text = budget + " / " + cost;
@@ -35,17 +33,14 @@ public class StationSim : MonoBehaviour
         stats[1].color = new Color(Mathf.Lerp(0, 1, ((float)mass / (float)SSMS.TotalMaxMass)), 1 - Mathf.Lerp(0, 1, ((float)mass / (float)SSMS.TotalMaxMass)), 0); 
         if (mass >= SSMS.TotalMaxMass) { stats[1].color = new Color(0.5f, 0, 0); }
 
-        /*//Net Power Production
-        stats[2].text = netpowprod + " W";
-        if (netpowprod > 0) { stats[2].color = new Color(0, 1, 0); }
-        else { stats[2].color = new Color(1, 0, 0); }*/
-
         //Morale
         stats[2].text = morale + "%";
         stats[2].color = new Color(1 - Mathf.Lerp(0, 1, ((float)morale / (float)100f)), Mathf.Lerp(0, 1, ((float)morale / (float)100f)), 0);
 
-        //COS
+        /*//COS
         stats[3].text = COS + "%";
-        stats[3].color = new Color(1 - Mathf.Lerp(0, 1, ((float)COS / (float)100f)), Mathf.Lerp(0, 1, ((float)COS / (float)100f)), 0);
+        stats[3].color = new Color(1 - Mathf.Lerp(0, 1, ((float)COS / (float)100f)), Mathf.Lerp(0, 1, ((float)COS / (float)100f)), 0);*/
+
+        
     }
 }
